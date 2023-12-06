0467291ebeba4be1fdbeb70f0b5afa16d9e8501f
Justin Pinkard <justin.d.pinkard@gmail.com>
2023-12-06 11:37:02 -0800

    story 4?

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,5 @@
 class Animal < ApplicationRecord
     has_many :sightings
+    validates :common_name, :scientific_binomial, presence: true
+    validates :common_name, :scientific_binomial, uniqueness: true
 end
--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Sighting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without latitude' do
+    scully = Sighting.create(
+      longitude: 'Predator',
+      date: 'Megan'
+    )
+    expect(scully.errors[:latitude]).to_not be_empty
+  end
+
+  it 'is not valid without longitude' do
+    scully = Sighting.create(
+      latitude: 'Predator',
+      date: 'Megan'
+    )
+    expect(scully.errors[:longitude]).to_not be_empty
+  end
+
+  it 'is not valid without date' do
+    scully = Sighting.create(
+      latitude: 'Predator',
+      longitude: 'Megan'
+    )
+    expect(scully.errors[:date]).to_not be_empty
+  end
 end
